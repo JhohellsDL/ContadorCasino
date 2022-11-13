b04944e834b7bbb7d22973c219f18b76a3d2b8b8
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-11-13 17:19:08 -0500

    recycler view ok

--- app/src/main/java/com/example/contadorcasino/ActividadesPositivas.kt
+++ app/src/main/java/com/example/contadorcasino/ActividadesPositivas.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.contadorcasino.adapter.ItemAdapter
+import com.example.contadorcasino.adapter.NegativeAdapter
 import com.example.contadorcasino.adapter.PositiveAdapter
 import com.example.contadorcasino.data.Datasource
 import com.example.contadorcasino.databinding.ActivityActividadesPositivasBinding
+import com.example.contadorcasino.model.NegativeAction
 import com.example.contadorcasino.model.PositiveAction
 
 class ActividadesPositivas : AppCompatActivity() {
@@ -18,14 +20,43 @@ class ActividadesPositivas : AppCompatActivity() {
         binding = ActivityActividadesPositivasBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myDataset = Datasource().loadPositiveActions()
-        val recyclerView = binding.recyclerViewPositive
-        recyclerView.adapter = PositiveAdapter(this,{
-            onItemSelected(it)
-        }, myDataset)
-        recyclerView.setHasFixedSize(true)
+        val myPositiveDataset = Datasource().loadPositiveActions()
+        val myNegativeDataset = Datasource().loadNegativeActions()
+        val recyclerViewPositive = binding.recyclerViewPositive
+        val recyclerViewNegative = binding.recyclerViewNegative
+
+        recyclerViewPositive.adapter = PositiveAdapter(
+            context = this,
+            onClickAction = {
+                onItemPositiveSelected(it)
+            },
+            dataset = myPositiveDataset
+        )
+        recyclerViewPositive.setHasFixedSize(true)
+
+        recyclerViewNegative.adapter = NegativeAdapter(
+            context = this,
+            onClickAction = {
+                onItemNegativeSelected(it)
+            },
+            dataset = myNegativeDataset
+        )
+
+
     }
-    fun onItemSelected(elementolista: PositiveAction) {
-        Toast.makeText(this, "${this.resources.getString(elementolista.stringResourceId)}", Toast.LENGTH_SHORT).show()
+
+    fun onItemNegativeSelected(elementolista: NegativeAction) {
+        Toast.makeText(
+            this,
+            "${this.resources.getString(elementolista.stringResourceId)}",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    fun onItemPositiveSelected(elementolista: PositiveAction) {
+        Toast.makeText(
+            this,
+            "${this.resources.getString(elementolista.stringResourceId)}",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
---
+++ app/src/main/java/com/example/contadorcasino/adapter/NegativeAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.contadorcasino.adapter
+
+import android.content.Context
+import android.service.autofill.Dataset
+import android.service.autofill.OnClickAction
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contadorcasino.R
+import com.example.contadorcasino.model.NegativeAction
+
+class NegativeAdapter(
+    private val context: Context,
+    private val onClickAction: (NegativeAction) -> Unit,
+    private val dataset: List<NegativeAction>
+) : RecyclerView.Adapter<NegativeAdapter.NegativeViewHolder>(){
+
+    class NegativeViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        private val txtStringNegative: TextView = view.findViewById(R.id.textStringPositive)
+
+        fun render(
+            context: Context,
+            element: NegativeAction,
+            onClickListener: (NegativeAction) -> Unit
+        ){
+            txtStringNegative.text = context.resources.getString(element.stringResourceId)
+            itemView.setOnClickListener { onClickListener(element) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NegativeViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_card_actions,parent,false)
+        return NegativeViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: NegativeViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.render(context, item, onClickAction)
+    }
+
+    override fun getItemCount(): Int = dataset.size
+}
