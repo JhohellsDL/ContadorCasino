000f5184a3c7a3936ea5c1e17736713a74be4091
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-11-15 12:16:44 -0500

    sin btn de ir a galeria

--- app/src/main/java/com/example/contadorcasino/MainActivity.kt
+++ app/src/main/java/com/example/contadorcasino/MainActivity.kt
@@ -11,7 +11,7 @@ import com.example.contadorcasino.adapter.ItemAdapter
 import com.example.contadorcasino.data.Datasource
 import com.example.contadorcasino.databinding.ActivityMainBinding
 import com.example.contadorcasino.model.Persona
-
+// sin boton a galeria
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
